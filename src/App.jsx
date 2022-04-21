3cfc22999bc03add982db31d91fa4415298f63ae
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-04-21 15:57:24 +0530

    FEAT: added navbar comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Mockman from "mockman-js";
+import { NavBar } from "./components";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
+        {/* <Route path="/" element={<App />} /> */}
         <Route path="mockman" element={<Mockman colorScheme="dark" />} />
       </Routes>
     </div>
