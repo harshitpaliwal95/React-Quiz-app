45f0bacf6bfce442cfd37a4ea077755244a91e94
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-04-27 00:03:14 +0530

    FIX: typeScript error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Mockman from "mockman-js";
+
 import { NavBar } from "./components";
 import { Category, Home, Quiz, Result, Rules } from "./pages";
 
@@ -14,7 +14,6 @@ function App() {
         <Route path="rules" element={<Rules />} />
         <Route path="quiz" element={<Quiz />} />
         <Route path="result" element={<Result />} />
-        <Route path="mockman" element={<Mockman colorScheme="dark" />} />
       </Routes>
     </div>
   );
